96bd7be04a85c561e79c121f6e894961c75d1983
unknown <raje1reddy@gmail.com>
2019-07-31 20:53:42 +0100

    Test for overloading methods

---
+++ src/test/java/ATest.java
@@ -0,0 +1,24 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ATest {
+
+    @Test
+    public void foo_padmaja() {
+
+        A a = new A();
+        int result = a.foo(5);
+        int expResult = 10;
+        assertEquals(expResult, result);
+    }
+
+
+    @Test
+    public void foo_padmajachar() {
+        A b = new A();
+        char result = b.foo(20,30);
+        char expResult = 'a';
+        assertEquals(expResult, result);
+    }
+}
